c79af92a0d1fc2fa047aab0a9a4107f3453c003d
Ravi do Valle Luz <ravi.luz@usp.br>
2020-06-23 11:15:08 -0300

    Make app init; Set up some models files.

---
+++ src/main/java/com/telelearner/TelelearnerApplication.java
@@ -0,0 +1,13 @@
+package com.telelearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelelearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelelearnerApplication.class, args);
+	}
+
+}
